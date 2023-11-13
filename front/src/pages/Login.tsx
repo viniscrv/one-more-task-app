a3d18dbbda24e730a8c3d47e7379c55418b2e116
Vinícius <viniscrv@gmail.com>
2023-11-13 06:35:33 -0300

    feat: validações de formulario

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -1,24 +1,92 @@
+import { z } from "zod";
 import styles from "./Login.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { api } from "../lib/axios";
+import { AxiosError } from "axios";
+import { useState } from "react";
+
+const loginFormSchema = z.object({
+  email: z.string().email({ message: "E-mail inválido" }),
+  password: z
+    .string()
+    .min(5, { message: "Sua senha deve conter ao menos 6 caracteres" }),
+});
+
+type LoginFormData = z.infer<typeof loginFormSchema>;
 
 const Login = () => {
-  function login() {}
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(loginFormSchema),
+  });
 
-  function redirectToRegister() {}
+  const [invalidCredentials, setInvalidCredentials] = useState(false);
+
+  const navigate = useNavigate();
+
+  function submitLogin({ email, password }: LoginFormData) {
+    handleLogin(email, password);
+  }
+
+  async function handleLogin(email: string, password: string) {
+    try {
+      const { data } = await api.post("/login", {
+        email,
+        password,
+      });
+
+      localStorage.setItem("token", JSON.stringify(data.token));
+      api.defaults.headers.Authorization = `Bearer ${data.token}`;
+
+      navigate("/tasks");
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        setInvalidCredentials(true);
+        return console.log(err.response.data.message);
+      }
+    }
+  }
 
   return (
     <div className={styles.login}>
-      <form>
+      <form onSubmit={handleSubmit(submitLogin)}>
         <div>
           <label>Email</label>
-          <input type="text" placeholder="Digite seu e-mail" />
+          <input
+            type="text"
+            placeholder="Digite seu e-mail"
+            {...register("email")}
+          />
+          {errors.email && (
+            <span className="invalid">{errors.email.message}</span>
+          )}
         </div>
         <div>
-          <label>Password</label>
-          <input type="text" placeholder="Digite seu password" />
+          <label>Senha</label>
+          <input
+            type="text"
+            placeholder="Digite seu password"
+            {...register("password")}
+          />
+          {errors.password && (
+            <span className="invalid">{errors.password.message}</span>
+          )}
         </div>
 
-        <button>Entrar</button>
+        {invalidCredentials ?? (
+          <div>
+            <span className="invalid">Credenciais inválidas</span>
+          </div>
+        )}
+
+        <button type="submit" disabled={isSubmitting}>
+          Entrar
+        </button>
         <Link to="/register">Cadatrar-se</Link>
       </form>
     </div>
--- front/src/pages/Register.tsx
+++ front/src/pages/Register.tsx
@@ -1,27 +1,96 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./Login.module.css";
 import { Link } from "react-router-dom";
+import { api } from "../lib/axios";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { AxiosError } from "axios";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const loginFormSchema = z.object({
+  username: z.string(),
+  email: z.string().email({ message: "E-mail inválido" }),
+  password: z
+    .string()
+    .min(5, { message: "Sua senha deve conter ao menos 6 caracteres" }),
+});
+
+type LoginFormData = z.infer<typeof loginFormSchema>;
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(loginFormSchema),
+  });
+
+  function submitRegister({ username, email, password }: LoginFormData) {
+    handleRegister(username, email, password);
+  }
 
   const navigate = useNavigate();
 
-  function register() {}
+  async function handleRegister(
+    username: string,
+    email: string,
+    password: string
+  ) {
+    try {
+      const { data } = await api.post("/register", {
+        username,
+        email,
+        password,
+      });
+
+      localStorage.setItem("token", JSON.stringify(data.token));
+      api.defaults.headers.Authorization = `Bearer ${data.token}`;
+
+      navigate("/tasks");
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        return console.log(err.response.data.message);
+      }
+    }
+  }
 
   return (
     <div className={styles.register}>
-      <form>
+      <form onSubmit={handleSubmit(submitRegister)}>
         <div>
-          <label>Username</label>
-          <input type="text" placeholder="Digite seu username" />
+          <label>Nome de usuário</label>
+          <input
+            type="text"
+            placeholder="Digite seu username"
+            {...register("username")}
+          />
+          {errors.username && (
+            <span className="invalid">{errors.username.message}</span>
+          )}
         </div>
         <div>
           <label>Email</label>
-          <input type="text" placeholder="Digite seu e-mail" />
+          <input
+            type="text"
+            placeholder="Digite seu e-mail"
+            {...register("email")}
+          />
+          {errors.email && (
+            <span className="invalid">{errors.email.message}</span>
+          )}
         </div>
         <div>
-          <label>Password</label>
-          <input type="text" placeholder="Digite seu password" />
+          <label>Senha</label>
+          <input
+            type="text"
+            placeholder="Digite seu password"
+            {...register("password")}
+          />
+          {errors.password && (
+            <span className="invalid">{errors.password.message}</span>
+          )}
         </div>
 
         <button>Registrar-se</button>
